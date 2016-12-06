ba0da95889287b474ba7efeb27ee1073e51474d6
渡辺太一 <watanabetaichi@watanabetaichi-no-MacBook-Air-2.local>
2016-12-06 17:40:19 +0900

    サイドナビの高さ取得

---
+++ js/index.js
@@ -0,0 +1,10 @@
+$(function() {
+  var sidenav = $('.content2');
+  var sidenav_offset = sidenav.offset();
+  var sidenav_outerHeight = sidenav.outerHeight();
+
+  console.log(sidenav_offset);
+  console.log(sidenav_outerHeight);
+
+  
+});
