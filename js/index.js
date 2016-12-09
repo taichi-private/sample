a83ca4db788b50e2510cffbc0bf4afe843b87a1b
渡辺太一 <watanabetaichi@watanabetaichi-no-MacBook-Air-2.local>
2016-12-09 23:11:54 +0900

    jsno

--- js/index.js
+++ js/index.js
@@ -4,24 +4,25 @@ $(function() {
   var sn_off_t = sn.offset().top;
   var mc = $('.main_content');
   var as = $('aside');
-  //課題
-  var sc_bottom = wd.height() - mc.outerHeight();
+  var sc_bottom = mc.outerHeight() - sn.outerHeight();
 
   console.log(sc_bottom);
-  
 
   $(window).scroll(function () {
     console.log(wd.scrollTop());
     if(sn_off_t < wd.scrollTop()){
       as.css('height',mc.outerHeight());
       sn.css('position','fixed');
-      //課題
+      sn.css('top',85);
+      sn.css('bottom','');
       if(sc_bottom < wd.scrollTop()) {
-        mc.css('background','red');
+        sn.css('position','absolute');
+        sn.css('bottom',0);
+        sn.css('top','');
       }
     } else {
       as.css('height','');
       sn.css('position','');
-    } 
+    }
   });
 });
